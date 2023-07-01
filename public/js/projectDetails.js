f336b76cb97544d9a6dad3faf0266670d0c0a9a4
shubhamdhiman <shubhamdhiman00.sd@gmail.com>
2023-07-01 15:27:59 +0530

    done with functionality

--- public/js/projectDetails.js
+++ public/js/projectDetails.js
@@ -1,60 +1,79 @@
-console.log("working in Project Details page");
+// const issueModel = require("../../models/issueModel");
 
+// Bringing data from ejs to js file
 const dataFromServer = document.getElementById("dataFromServer");
-
 const dataParse = JSON.parse(dataFromServer.value);
-// const dataParse = dataFromServer.value;
-
 console.log(dataParse.issues);
 
-const bugSection = document.getElementById("bugSection");
-const filterBtn = document.getElementById("filterButton");
-function filteredFunction(arr) {
-  // console.log("consoling from filteredFunction",arr)
-  if (arr.length > 0) {
-    for (i = 0; i < arr.length; i++) {
-      // console.log(arr[i])
 
-      let newDiv = document.createElement("div");
-      newDiv.className = "bug";
-      let filteredItem = `
-                        <p class="title">Title: <span class="name">${arr[i].title}</span></p>
-                        <p class="description">Description: ${arr[i].description}</p>
-                        <p class="label">Label: ${arr[i].label} </p>
-                        <p class="author">Author: ${arr[i].issueAuthor}</p>
+const bugSection = document.getElementById("bugSection");
+
+// Function to display issues after filtering
+function filteredFunction(arr) {
+    bugSection.innerHTML = "";
+
+    // If there is no array just display notification else print the issues
+    if (arr.length > 0) {
+        for (i = 0; i < arr.length; i++) {
+            let newDiv = document.createElement("div");
+            newDiv.className = "bug";
+            let filteredItem = `
+            <p class="title">Title: <span class="name">${arr[i].title}</span></p>
+            <p class="description">Description: ${arr[i].description}</p>
+            <p class="label">Label: ${arr[i].label} </p>
+            <p class="author">Author: ${arr[i].issueAuthor}</p>
             
             `;
-      newDiv.innerHTML = filteredItem;
-      bugSection.append(newDiv);
+            newDiv.innerHTML = filteredItem;
+            bugSection.append(newDiv);
+        }
+    } else {
+        let newDiv = document.createElement("div");
+        newDiv.className = "noIssueFound";
+        newDiv.innerHTML = "<h1>No Bug Found</h1>";
+        bugSection.append(newDiv);
     }
-  } else {
-    let newDiv = document.createElement("div")
-    newDiv.className = "noIssueFound"
-    newDiv.innerHTML = "<h1>No Bug Found</h1>"
-    bugSection.append(newDiv)
-    // console.log("not found");
-  }
 }
+
+
+// Filter Issue via Label
+const filterBtn = document.getElementById("filterButton");
 filterBtn.addEventListener("click", function () {
   const filteredArr = [];
 
-  bugSection.innerHTML = "";
   const checkBoxes = document.querySelectorAll("input[type='checkbox']");
-  // console.log(checkBoxes)
+
   for (let i = 0; i < checkBoxes.length; i++) {
-    // console.log(checkBoxes[i].value)
     if (checkBoxes[i].checked) {
       console.log(checkBoxes[i].checked);
-      // console.log("inside")
       for (let j = 0; j < dataParse.issues.length; j++) {
-        // console.log("inside loop also")
         if (dataParse.issues[j].label === checkBoxes[i].value) {
           filteredArr.push(dataParse.issues[j]);
         }
       }
-      //     // filteredArr.push(checkBoxes[i])
     }
   }
-  // console.log(filteredArr)
   filteredFunction(filteredArr);
 });
+
+
+
+// Find Issue via Author Filter
+const filterBtnByAuthor = document.getElementById("filterButtonByAuthor");
+filterBtnByAuthor.addEventListener("click", function () {
+  const optionValue = document.getElementById("label");
+  const author = dataParse.issues.filter(
+    (issue) => issue.issueAuthor === optionValue.value
+  );
+  filteredFunction(author);
+});
+
+
+// Find issue via searching by title or description
+
+const searchBtn = document.getElementById("searchButton")
+searchBtn.addEventListener("click",function(){
+    const searchValue = document.getElementById("searchBar").value.toLowerCase().trim()
+    const filtered = dataParse.issues.filter((issue)=>issue.title.toLowerCase().includes(searchValue) || issue.description.toLowerCase().includes(searchValue))
+    filteredFunction(filtered)
+})

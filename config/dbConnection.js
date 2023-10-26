ac30d97a99bf6457d5b6a0ff75a9e7dcda801093
Shubham <shubhamdhiman00.sd@gmail.com>
2023-10-26 09:00:37 +0530

    Update dbConnection.js

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,10 +1,10 @@
-require('dotenv').config();
+
 const mongoose = require("mongoose")
 
 // Making Connecting with mongoDB
 const connectDb = async ()=>{
     try{
-        const connect = await mongoose.connect(process.env.MONGODB_URI)
+        const connect = await mongoose.connect("mongodb+srv://admin:admin@shubhamcluster.ieolx9t.mongodb.net/IssueTracker?retryWrites=true&w=majority")
         console.log("Database Connected: ", connect.connection.name)
     }
     catch(err){
